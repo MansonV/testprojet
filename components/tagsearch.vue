e15f7c6700d382998d863ac9d6fd71e2c619a30a
MansonV <59490357+MansonV@users.noreply.github.com>
2020-01-04 00:05:24 +0100

    Add files via upload

---
+++ components/tagsearch.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class= "tagsearch">
+        <h1> Ajoute un tag pour trouver un jeu</h1>
+        <label> Tag <input type="text" v-model="jeu.tag" /></label>
+        <button> + </button>
+         <p> Nombre de jeux comportant ce tag : {{ counter }}</p>
+    </div>
+</template>
+
+<script>
+
+export default {
+    data () {
+        return {
+            jeu: []
+        }
+    },
+    computed: {
+        counter () {
+            return this.jeu.length
+        }
+    }
+}
+    methods: {}
+</script>
+
+<style>
+</style>
